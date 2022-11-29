89b6097d2e3a4cafe66e72777cbb7020c61d4fbb
e_szeliga <eszeliga@student.agh.edu.pl>
2022-11-29 18:59:06 +0100

    Add models for restaurant and table

---
+++ backend/Restaurant-System.DataAcessLayer/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿namespace Restaurant_System.DataAcessLayer.Models
+{
+    public class Restaurant
+    {
+        int Id;
+
+        string Name;
+
+        string City;
+
+        string Address;
+
+        public Restaurant(int id, string name, string city, string address)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+            Address = address;
+        }
+    }
+}
---
+++ backend/Restaurant-System.DataAcessLayer/Models/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_System.DataAcessLayer.Models
+{
+    public class Table
+    {
+        int Id;
+
+        int NumberOfTable;
+
+        int NumberOfSits;
+
+        public ICollection<Restaurant> Restaurants;
+
+        public Table(int numberOfTable, int numberOfSits, ICollection<Restaurant> restaurants)
+        {
+            NumberOfTable = numberOfTable;
+            NumberOfSits = numberOfSits;
+            Restaurants = restaurants;
+        }
+    }
+}
