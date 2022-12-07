d708e17fb85a79561f0b4587730e75c80c1754c7
e_szeliga <eszeliga@student.agh.edu.pl>
2022-12-08 00:27:00 +0100

    #RS-38 Add draft for table with product list

--- frontend/res-sys/src/Components/Products.jsx
+++ frontend/res-sys/src/Components/Products.jsx
@@ -1,5 +1,40 @@
 import { useEffect, useState } from "react";
 import { myFetch } from "../neew";
+import * as React from 'react';
+import '../Component_CSS/Product.css';
+// export default function ListProduct({}) {
+//   const [products,setProducts] = useState([]);
+//   useEffect(()=>{
+//     async function fetchProductList(){
+//       const data = await myFetch("https://localhost:7117/ProductList");
+//       console.log(data);
+//       setProducts(data);
+//     }
+//     fetchProductList();
+//   },[])
+//     return (
+//       <div>
+//       <h1>GROCERY LIST GENERATOR</h1>
+//     <>
+//       {
+
+        
+//         products.map((product) => {
+//          return (
+//           <div> 
+//             <li key={product.id}>{product.productName} --- {product.actualNumber}[{product.unit}]</li>
+//           </div>
+//         )
+//         })
+
+
+//       } 
+
+
+//     </>
+//       </div>);
+// }
+
 
 export default function ListProduct({}) {
   const [products,setProducts] = useState([]);
@@ -13,18 +48,33 @@ export default function ListProduct({}) {
   },[])
     return (
       <div>
-      <p>Nazwa produktu --- Aktualna ilość[jednostka]</p>
+      <h1>GROCERY LIST GENERATOR</h1>
     <>
       {
         
         products.map((product) => {
          return (
-          <div> 
-            <li key={product.id}>{product.productName} --- {product.actualNumber}[{product.unit}]</li>
-          </div>
+
+    <div className="App">
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Nazwa</th>
+          <th>Aktualna ilość</th>
+        </tr>
+        <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.productName}</td>
+              <td>{product.actualNumber}</td>
+        </tr>       
+      </table>
+    </div>
         )
-        })
-      }
+        
+      })
+      } 
+
+
     </>
       </div>);
 }
