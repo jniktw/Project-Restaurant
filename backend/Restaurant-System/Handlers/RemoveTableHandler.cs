6d542befe1ef9385cf6eae3ab2e88d548bb2d4be
Kamila Gadek <gadek@student.agh.edu.pl>
2022-12-08 23:56:27 +0100

    VET-44 Add functionality remove table handler

---
+++ backend/Restaurant-System/Handlers/RemoveTableHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Restaurant_System.DataAcessLayer;
+using Restaurant_System.DataAcessLayer.Models;
+
+namespace Restaurant_System.Handlers
+{
+    public class RemoveTableHandler : IRequestHandler<RemoveTableCommand>
+    {
+        private readonly IContext _context;
+
+        public RemoveTableHandler(IContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Unit> Handle(RemoveTableCommand request, CancellationToken cancellationToken)
+        {
+            _context.Products.Where(t => t.IdTable.Equals(request.IdTable)).ExecuteDeleteAsync();
+            _context.SaveChangesAsync();
+            return Unit.Task;
+        }
+    }
+
+    public record RemoveTableCommand(int IdTable) : IRequest;
+}
