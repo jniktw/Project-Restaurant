c0d7560b2be50be169aee76ef45839a34f8c132d
e_szeliga <eszeliga@student.agh.edu.pl>
2022-12-09 00:18:34 +0100

    #RS-38 Add css with style to product component html

--- frontend/res-sys/src/Components/Products.jsx
+++ frontend/res-sys/src/Components/Products.jsx
@@ -2,39 +2,6 @@ import { useEffect, useState } from "react";
 import { myFetch } from "../neew";
 import * as React from 'react';
 import '../Component_CSS/Product.css';
-// export default function ListProduct({}) {
-//   const [products,setProducts] = useState([]);
-//   useEffect(()=>{
-//     async function fetchProductList(){
-//       const data = await myFetch("https://localhost:7117/ProductList");
-//       console.log(data);
-//       setProducts(data);
-//     }
-//     fetchProductList();
-//   },[])
-//     return (
-//       <div>
-//       <h1>GROCERY LIST GENERATOR</h1>
-//     <>
-//       {
-
-        
-//         products.map((product) => {
-//          return (
-//           <div> 
-//             <li key={product.id}>{product.productName} --- {product.actualNumber}[{product.unit}]</li>
-//           </div>
-//         )
-//         })
-
-
-//       } 
-
-
-//     </>
-//       </div>);
-// }
-
 
 export default function ListProduct({}) {
   const [products,setProducts] = useState([]);
@@ -47,39 +14,48 @@ export default function ListProduct({}) {
     fetchProductList();
   },[])
     return (
+      
       <div>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=PT+Serif&display=swap');
+        </style>
+
       <h1>GROCERY LIST GENERATOR</h1>
+      <div className="App">
+      <table>
+        <tr>
+          <th>Item</th>
+          <th>Actual number</th>
+          <th>Required number</th>
+          <th>Unit</th>
+        </tr>  
     <>
       {
         
         products.map((product) => {
          return (
-
-    <div className="App">
-      <table>
-        <tr>
-          <th>Id</th>
-          <th>Nazwa</th>
-          <th>Aktualna ilość</th>
-        </tr>
-        <tr key={product.id}>
-              <td>{product.id}</td>
-              <td>{product.productName}</td>
-              <td>{product.actualNumber}</td>
-        </tr>       
-      </table>
-    </div>
+          <tr key={product.id}>
+          <td>{product.productName}</td>
+          <td>{product.actualNumber}</td>
+          <td>{product.requiredNumber}</td>
+          <td>{product.unit}</td>
+          </tr>    
         )
-        
       })
       } 
-
-
     </>
-      </div>);
-}
+    </table>
+      <div className="Buttons">
+        <a href="#">Add items</a>
+        <a href="#">Remove items</a>
+        <a href="#">Update items</a>
+      </div>
 
+    </div>
+    </div>
+    );
 
+    }
 
 
 
