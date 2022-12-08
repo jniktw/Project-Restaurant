a0aeb0acdee327ad4503c44adc9d66c64bd2520e
Kamila Gadek <gadek@student.agh.edu.pl>
2022-12-09 00:08:45 +0100

    RS-44 Add list table functionality with handler

---
+++ backend/Restaurant-System/Handlers/ListTableHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Restaurant_System.DataAcessLayer;
+using Restaurant_System.DataAcessLayer.Models;
+
+namespace Restaurant_System.Handlers
+{
+    public class ListTableHandler : IRequestHandler<ListTableCommand,IEnumerable<Product>>
+    {
+        private readonly IContext _context;
+
+        public ListTableHandler(IContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Table>> Handle(ListTableCommand request, CancellationToken cancellationToken)
+        {
+            var Table = await _context.Tables.ToArrayAsync();
+            if (Table == null)
+            {
+                return null;
+            }
+            using StreamWriter file = new("Table.txt");
+            foreach (var table in Table)
+            {
+                string reservation = "not reserved";
+                if (table.IsReserved)
+                {
+                    reservation = "reserved";
+                }
+                await file.WriteLineAsync(table.IdTable + " Table in " + table.IdRoom + "room with " + table.NumberOfSeats + " seats" + "is " + reservation;
+            }
+            return Table;
+        }
+    }
+
+    public record ListTableCommand : IRequest<IEnumerable<Table>>; 
+}
