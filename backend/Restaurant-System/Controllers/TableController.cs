55d96b5d7fb034aa22e9a354df2f89b1ede89713
Kamila Gadek <gadek@student.agh.edu.pl>
2022-12-08 23:50:46 +0100

    RS-44 Add Controller for Table model and first controller functionality with adding tables

---
+++ backend/Restaurant-System/Controllers/TableController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant_System.DataAcessLayer.Models;
+using Restaurant_System.Handlers;
+
+
+namespace Restaurant_System.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TableController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public TableController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPut()]
+
+        public async Task Put([FromBody] UpdateTableCommand command)
+        {
+            await _mediator.Send(command);
+        }
+        
+        [HttpGet()]
+
+        public async Task<IEnumerable<Table>> Get()
+        {
+            return await _mediator.Send(new ListTableCommand());
+        }
+        [HttpPost()]
+
+        public async Task Post( [FromBody] AddListCommand command )
+        {
+            await _mediator.Send(command);
+        }
+
+        [HttpDelete()]
+        public async Task Delete([FromBody] RemoveTableCommand command)
+        {
+            await _mediator.Send(command);
+        }
+    }
+}
---
+++ backend/Restaurant-System/Handlers/AddTableHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Restaurant_System.DataAcessLayer;
+using Restaurant_System.DataAcessLayer.Models;
+
+namespace Restaurant_System.Handlers
+{
+    public class AddTableHandler : IRequestHandler<AddTableCommand>
+    {
+        private readonly IContext _context;
+
+        public AddTableHandler(IContext context)
+        {
+            _context = context;
+        }
+        
+        public Task<Unit> Handle(AddTableCommand request, CancellationToken cancellationToken)
+        {
+            var table = new Table(request.IdRestaurant, request.IdRoom, request.NumberOfSeats, request.IsReserved);
+            _context.Tables.Add(table);
+            _context.SaveChangesAsync();
+            return Unit.Task;
+        }
+    }
+
+    public record AddTableCommand(int IdRestaurant, int IdRoom, int NumberOfSeats, bool IsReserved) : IRequest;
+}
