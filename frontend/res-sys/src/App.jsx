35fd898cf72e555ea1f4e05ed9c0860428a3c3d0
e_szeliga <eszeliga@student.agh.edu.pl>
2022-12-16 16:07:47 +0100

    #RS-27 Add front with not working post

--- frontend/res-sys/src/App.jsx
+++ frontend/res-sys/src/App.jsx
@@ -1,10 +1,15 @@
 //import logo from './logo.svg';
 //import './App.css';
-
+import React from "react";
 import ListProduct from "./Components/Products";
-
-export default function App(){
-    return (    
-        <><ListProduct/></>
+import './App.css'
+  
+class App extends React.Component {
+  render() {
+    return (
+        <ListProduct></ListProduct>
     );
+  }
 }
+
+export default App;
--- frontend/res-sys/src/Components/Products.jsx
+++ frontend/res-sys/src/Components/Products.jsx
@@ -2,17 +2,66 @@ import { useEffect, useState } from "react";
 import { myFetch } from "../neew";
 import * as React from 'react';
 import '../Component_CSS/Product.css';
+import Modal from './modal'
 
-export default function ListProduct({}) {
-  const [products,setProducts] = useState([]);
-  useEffect(()=>{
-    async function fetchProductList(){
-      const data = await myFetch("https://localhost:7117/ProductList");
-      console.log(data);
-      setProducts(data);
-    }
-    fetchProductList();
-  },[])
+
+export default class ListProduct extends React.Component  {
+  state = {
+    show: false
+  };
+
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  // setProducts() {
+  //   async function fetchProductList(){
+  //     const data = await myFetch("https://localhost:7117/ProductList");
+  //     console.log(data);
+  //     this.products = data;
+  //   }
+  //   fetchProductList();
+  // }
+
+
+
+  constructor(props) {
+    super(props);
+    this.products = [
+      {
+        "id": 2,
+        "productName": "maliny",
+        "actualNumber": 1,
+        "requiredNumber": 0,
+        "unit": "kg"
+      },
+      {
+        "id": 3,
+        "productName": "jajka",
+        "actualNumber": 5,
+        "requiredNumber": 30,
+        "unit": "szt"
+      },
+      {
+        "id": 4,
+        "productName": "cukier",
+        "actualNumber": 0,
+        "requiredNumber": 10,
+        "unit": "kg"
+      },
+      {
+        "id": 5,
+        "productName": "string",
+        "actualNumber": 0,
+        "requiredNumber": 0,
+        "unit": "string"
+      }
+    ];
+  }
+
+  render() {
     return (
       
       <div>
@@ -32,7 +81,7 @@ export default function ListProduct({}) {
     <>
       {
         
-        products.map((product) => {
+        this.products.map((product) => {
          return (
           <tr key={product.id}>
           <td>{product.productName}</td>
@@ -46,74 +95,72 @@ export default function ListProduct({}) {
     </>
     </table>
       <div className="Buttons">
-        <button onClick={callComp}>Add items</button>
+        <button onClick={e => {this.showModal(e);}}>Add items</button>
         <a href="#">Remove items</a>
         <a href="#">Update items</a>
       </div>
 
     </div>
+      <Modal onClose={this.showModal} show={this.state.show}>
+      <EmployeeComponent></EmployeeComponent>
+      </Modal>
     </div>
     );
-
-    }
+  }
+  }
     
 
-    class EmployeeComponent extends React.Component {
-      constructor(props) {
-          super(props);
-          this.state = {
-            message: ""
+class EmployeeComponent extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        message: ""
+      };
+    }
+
+    render(){
+      return(
+        <div classname = "addForm">
+        <h2>Please Enter Product Details...</h2>
+        <p>
+          <label>Product Name : <input type="text" ref="ProductName"></input></label>
+        </p>
+        <p>
+          <label>Product Actual Number : <input type="number" ref="ActualNumber"></input></label>
+        </p>
+        <p>
+          <label>Product Required Number : <input type="number" ref="RequiredNumber"></input></label>
+        </p>
+        <p>
+          <label>Product Unit : <input type="text" ref="Unit"></input></label>
+        </p>
+          <button onClick={this.onCreateEmployee}>Create</button>
+          </div>
+      )
+    }
+
+    onCreateEmployee=()=>{
+      let empInfo={
+        ProductName:this.refs.ProductName.value,
+        ActualNumber:this.refs.ActualNumber.value,
+        RequiredNumber:this.refs.RequiredNumber.value,
+        Unit:this.refs.Unit.value
+      
           };
-        }
+          fetch('https://localhost:7117/ProductList',{
+            method: 'POST',
+            headers:{'Content-type':'application/json'},
+              body: empInfo
+          }).then(r=>r.json()).then(res=>{
+            if(res){
+              this.setState({message:'New Employee is Created Successfully'});
+            }
+          });
+    }
 
-        render(){
-          return(
-            <div classname = "addForm">
-            <h2>Please Enter Product Details...</h2>
-            <p>
-              <label>Product Name : <input type="text" ref="ProductName"></input></label>
-            </p>
-            <p>
-              <label>Product Actual Number : <input type="number" ref="ActualNumber"></input></label>
-            </p>
-            <p>
-              <label>Product Required Number : <input type="number" ref="RequiredNumber"></input></label>
-            </p>
-            <p>
-              <label>Product Unit : <input type="text" ref="Unit"></input></label>
-            </p>
-              <button onClick={this.onCreateEmployee}>Create</button>
-              </div>
-          )
-        }
-
-        onCreateEmployee=()=>{
-          let empInfo={
-            ProductName:this.refs.ProductName.value,
-            ActualNumber:this.refs.ActualNumber.value,
-            RequiredNumber:this.refs.RequiredNumber.value,
-            Unit:this.refs.Unit.value
-          
-              };
-              fetch('https://localhost:7117/ProductList',{
-      method: 'POST',
-      headers:{'Content-type':'application/json'},
-        body: empInfo
-    }).then(r=>r.json()).then(res=>{
-      if(res){
-        this.setState({message:'New Employee is Created Successfully'});
-      }
-    });
-          }
+}
 
 
-
-      }
-
-      function callComp(){
-        const element = <EmployeeComponent></EmployeeComponent>
-        return element;
-      }
       
     
   //   class EmployeeComponent extends React.Component {
