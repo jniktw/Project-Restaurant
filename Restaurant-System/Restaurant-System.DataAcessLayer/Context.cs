4f8c450a91ff87edbd8f1ed6706f4bb713241ca5
e_szeliga <eszeliga@student.agh.edu.pl>
2022-12-23 10:39:05 +0100

    #RS-63 Change structure  of project to Razor

---
+++ Restaurant-System/Restaurant-System.DataAcessLayer/Context.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Restaurant_System.DataAcessLayer.Models;
+
+namespace Restaurant_System.DataAcessLayer
+{
+
+    public interface IContext
+    {
+        DbSet<Product> Products { get; }
+        DbSet<Restaurant> Restaurants { get; }
+        DbSet<Table> Tables { get; }
+        DbSet<Reservation> Reservations { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+    public class Context : DbContext, IContext
+    {
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Restaurant> Restaurants { get; set; } = null!;
+        public DbSet<Table> Tables { get; set; } = null!;
+        public DbSet<Reservation> Reservations { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Filename=Database.sqlite");
+    }
+}
---
+++ Restaurant-System/Restaurant-System.DataAcessLayer/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant_System.DataAcessLayer.Models;
+
+public class Reservation
+{
+    [Key]
+    public int ReservationId { get; set; }
+    public DateTime ReservationDate { get; set; }
+
+    public Table Table { get; set; }
+
+    public Reservation(DateTime reservationDate)
+    {
+        ReservationDate = reservationDate;
+    }
+
+}
